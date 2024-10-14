befbee36db141afcfd64d5f7cf6502faf915be78
JaffarsonJS <jaffarson2000@gmail.com>
2024-10-14 10:48:44 +0530

    updated cors policy

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
